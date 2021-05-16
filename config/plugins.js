e6e24e5d513ccaa697817923f5f68c90f70e0d5b
Oleksii Demchenko <o.demchenko@wrenchtech.io>
2021-05-16 18:19:51 +0300

    init commit

---
+++ config/plugins.js
@@ -0,0 +1,20 @@
+module.exports = ({ env }) => ({
+  graphql: {
+    endpoint: '/graphql',
+    shadowCRUD: true,
+    playgroundAlways: false,
+    depthLimit: 7,
+    amountLimit: 100,
+    apolloServer: {
+      tracing: false,
+    },
+  },
+  upload: {
+    provider: 'cloudinary',
+    providerOptions: {
+      cloud_name: env('CLOUDINARY_NAME'),
+      api_key: env('CLOUDINARY_KEY'),
+      api_secret: env('CLOUDINARY_SECRET'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '5aaabe38-2e0e-4bc3-b6a9-20175ff94d4d'
+};
